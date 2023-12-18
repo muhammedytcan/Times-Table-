699f212cbd5025b5a6df6ad9c079501b00869f38
muhammedytcan <muhammedytcan@gmail.com>
2023-12-18 18:44:06 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Çarpım_Tablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine("-----------------------");
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
